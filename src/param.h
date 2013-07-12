d58015e5ac791caecdf0746879202904df8dd6fa
badalindia <badalindia88@gmail.com>
2013-07-12 12:24:49 +0530

    tag

--- src/param.h
+++ src/param.h
@@ -9,7 +9,7 @@
 #include <fstream>
 using namespace std;
 
-	#define TAG "Master" //tag that appends at the end of csv file names
+	#define TAG "DefaultKernel_BlockSize8" //tag that appends at the end of csv file names
 
 	typedef double ptype;	//precision type
 	
